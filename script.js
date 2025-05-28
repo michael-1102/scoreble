9cc48fd4f03f6de341eea64de4d511cbbfe22298
michael-1102 <michaelhanley0806@gmail.com>
2025-05-27 22:52:47 -0400

    added word list

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-import { WORDS } from "./words.js"
+import { ALL_WORDS } from "./words.js";
 
 let correctWord = "ghost";
 const BOARD_WIDTH = 15;
 const BOARD_LENGTH = BOARD_WIDTH;
 const NUM_TILES = BOARD_WIDTH * BOARD_LENGTH;
-const WORD_LENGTH = 5;
+
+const horizontalWrong = String.raw`<svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 40 40"
+          width="40"
+          height="40"
+          class="arrow"
+        >
+          <rect x="27.5" y="17" width="6" height="6" fill="gray"/>
+          <path d="M 28.5 18 v 4 l 4 -2 z" fill="transparent" stroke-width="0.2" stroke="black"></path>
+        </svg>`;
+const verticalWrong = String.raw`<svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 40 40"
+          width="40"
+          height="40"
+          class="arrow"
+        >
+          <rect x="17" y="27.5" width="6" height="6" fill="gray"/>
+          <path d="M 18 28.5 h 4 l -2 4 z" fill="transparent" stroke-width="0.2" stroke="black"></path>
+        </svg>`;
+
 
 const horizontalCorrect = String.raw`<svg
           xmlns="http://www.w3.org/2000/svg"
@@ -64,6 +85,9 @@ function print(str) {
 }
 
 function initBoard() {
+
+  //correctWord = ALL_WORDS[Math.floor(Math.random() * ALL_WORDS.length)];
+
   for (let i = 0; i < BOARD_WIDTH; i++) {
     for (let j = 0; j < BOARD_LENGTH; j++) {
       let tile = document.createElement("button");
@@ -73,13 +97,11 @@ function initBoard() {
     }
   }
   let center = Math.floor(NUM_TILES / 2);
-  let halfWordLength = Math.floor(WORD_LENGTH / 2);
-  for (let i = center - halfWordLength; i <= center + halfWordLength; i++)
+  let halfWordLength = Math.floor(correctWord.length / 2);
+  let offset = (correctWord.length - 1) % 2;
+  for (let i = center - halfWordLength; i <= center + halfWordLength - offset; i++)
     board.children[i].dataset.active = 'true';
   
-
-  //correctWord = WORDS[Math.floor(Math.random() * WORDS.length)];
-
   enableGuessing();
   startListening();
 }
@@ -117,11 +139,12 @@ function handleKey(e) {
     if (filledTiles.length == 0) {
       const guessTiles = board.querySelectorAll("[data-active='true']");
       guessTiles.forEach((tile) => {
+        tile.dataset.active = "false";
         if (tile.dataset.letter == null)
           delete tile.dataset.active;
       });
+      disableGuessing();
     }
-    disableGuessing();
   }
 }
 
@@ -141,15 +164,15 @@ function pressTile(tile) {
   tile.blur();
   if (guessing) return;
 
-  if (selectedTiles.length < WORD_LENGTH) {
-    print("Guess must be exactly " + WORD_LENGTH + " letters");
+  if (selectedTiles.length < correctWord.length) {
+    print("Guess must be exactly " + correctWord.length + " letters");
     return;
   }
 
   let inc; 
   if (nextGuessIsVertical)
     inc = BOARD_WIDTH
-  else
+  else 
     inc = 1;
 
   // check if word is connected to but not equal to existing word
@@ -158,6 +181,14 @@ function pressTile(tile) {
   selectedTiles.forEach((tile) => {
     if (tile.dataset.letter) {
       hasConnected = true;
+      if (nextGuessIsVertical) {
+        if (!tile.dataset.vertical)
+          hasDisconnected = true;
+      } else {
+        if (!tile.dataset.horizontal)
+          hasDisconnected = true;
+      }
+          
     } else {
       hasDisconnected = true;
     }
@@ -169,24 +200,25 @@ function pressTile(tile) {
   }
 
   if (!hasDisconnected) {
-    print("Guess must not entirely overlap existing word");
+    print("Already guessed");
     return;
   }
+
   
   // check if there is a letter before word
   let firstIndex = getIndex(selectedTiles[0])
-  if (firstIndex > 0) {
+  if (firstIndex - inc > 0) {
     if (board.children[firstIndex - inc].dataset.letter) {
-      print("Guess must be exactly " + WORD_LENGTH + " letters");
+      print("Guess must be exactly " + correctWord.length + " letters");
       return;
     }
   }
 
   // check if there is a letter after word
-  let lastIndex = getIndex(selectedTiles[WORD_LENGTH - 1])
+  let lastIndex = getIndex(selectedTiles[correctWord.length - 1])
   if (lastIndex < NUM_TILES - inc) {
     if (board.children[lastIndex + inc].dataset.letter) {
-      print("Guess must be exactly " + WORD_LENGTH + " letters");
+      print("Guess must be exactly " + correctWord.length + " letters");
       return;
     }
   }  
@@ -226,13 +258,13 @@ function selectTiles(e) {
 function highlightTiles(tile) {
   let tileIndex = getIndex(tile);
   if (nextGuessIsVertical) {
-    for (let i = tileIndex; i < tileIndex + WORD_LENGTH*BOARD_LENGTH; i+=BOARD_LENGTH) {
+    for (let i = tileIndex; i < tileIndex + correctWord.length*BOARD_LENGTH; i+=BOARD_LENGTH) {
       if (i % BOARD_LENGTH < tileIndex % BOARD_LENGTH) break;
       board.children[i].dataset.selected = "true";
       selectedTiles.push(board.children[i]);
     }
   } else {
-    for (let i = tileIndex; i < tileIndex + WORD_LENGTH; i++) {
+    for (let i = tileIndex; i < tileIndex + correctWord.length; i++) {
       if (i % BOARD_LENGTH < tileIndex % BOARD_WIDTH) break;
       board.children[i].dataset.selected = "true";
       selectedTiles.push(board.children[i]);
@@ -266,23 +298,57 @@ function deleteKey() {
 function submitGuess() {
   if (!guessing) return;
   const guessTiles = board.querySelectorAll("[data-letter][data-active='true']");
-  if (guessTiles.length < WORD_LENGTH) {
+  if (guessTiles.length < correctWord.length) {
     print("Not enough letters");
     return;
   }
   
-
   const guess = Array.from(guessTiles).reduce((word, tile) => {
     return word + tile.dataset.letter;
   }, "");
 
-
-  if (!WORDS.includes(guess)) {
-    print("Invalid word");
+  if (binarySearch(ALL_WORDS, guess) < 0) {
+    print(guess + " is not in word list");
     return;
   }
 
-  //TODO: check if connected words are invalid
+  // for vertical word, check to left and right
+  // for horizontal word, check above and below
+  let inc; 
+  if (nextGuessIsVertical)
+    inc = 1
+  else 
+    inc = BOARD_WIDTH;
+
+  for (let i = 0; i < guessTiles.length; i++) {
+    let tile = guessTiles[i];
+    let word = tile.dataset.letter;
+
+    //TODO: check if out of bounds
+    let prevTile = board.children[getIndex(tile) - inc];
+    let num = 2;
+  
+    while (prevTile.dataset.letter) {
+      word = prevTile.dataset.letter + word;
+      prevTile = board.children[getIndex(tile) - num * inc];
+      num++;
+    }
+  
+    let nextTile = board.children[getIndex(tile) + inc];
+    num = 2;
+    while (nextTile.dataset.letter) {
+      word += nextTile.dataset.letter;
+      nextTile = board.children[getIndex(tile) + num * inc];
+      num++;
+    }
+
+    if (word.length <= 1) continue;
+
+    if (binarySearch(ALL_WORDS, word) < 0) {
+      print(word + " is not in word list");
+      return;
+    }
+  }
 
   // guess submitted
   nextGuessIsVertical = !nextGuessIsVertical;
@@ -336,20 +402,54 @@ function getKey(char) {
 
 function wrongLetter(key, tile) {
   if (key.dataset.state != "correct" && key.dataset.state != "wrong-spot") key.dataset.state = "wrong";
+  if (nextGuessIsVertical) {
+    tile.insertAdjacentHTML("beforeend", horizontalWrong);
+    tile.dataset.horizontal = "true";
+  } else {
+    tile.insertAdjacentHTML("beforeend", verticalWrong);
+    tile.dataset.vertical = "true";
+  }
 }
+
 
 function correctLetter(key, tile) {
   key.dataset.state = "correct";
-  if (nextGuessIsVertical)
+  if (nextGuessIsVertical) {
     tile.insertAdjacentHTML("beforeend", horizontalCorrect);
-  else
+    tile.dataset.horizontal = "true";
+  } else {
     tile.insertAdjacentHTML("beforeend", verticalCorrect);
+    tile.dataset.vertical = "true";
+  }
 }
 
 function wrongSpotLetter(key, tile) {
   if (key.dataset.state != "correct") key.dataset.state = "wrong-spot";
-  if (nextGuessIsVertical)
+  if (nextGuessIsVertical) {
     tile.insertAdjacentHTML("beforeend", horizontalWrongSpot);
-  else
+    tile.dataset.horizontal = "true";
+  } else {
     tile.insertAdjacentHTML("beforeend", verticalWrongSpot);
+    tile.dataset.vertical = "true";
+  }
+}
+
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] == val) {
+      return mid;
+    }
+
+    if (val < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
 }
